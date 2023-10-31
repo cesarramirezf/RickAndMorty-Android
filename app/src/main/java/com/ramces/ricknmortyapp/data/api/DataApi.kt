d1024bdd078bf6004021d302e6d39d673c8c9ca0
Cesar Ramirez Filomena <cesarramirezfilomena@MBP-de-Cesar.homenet.telecomitalia.it>
2023-10-31 12:05:08 +0100

    -Solve Error deprecate plugin Android-extensions and parcelize - Android.

--- app/src/main/java/com/ramces/ricknmortyapp/data/api/DataApi.kt
+++ app/src/main/java/com/ramces/ricknmortyapp/data/api/DataApi.kt
@@ -1,7 +1,7 @@
 package com.ramces.ricknmortyapp.data.api
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class DataApi(
--- app/src/main/java/com/ramces/ricknmortyapp/data/api/DataResponseApi.kt
+++ app/src/main/java/com/ramces/ricknmortyapp/data/api/DataResponseApi.kt
@@ -1,8 +1,8 @@
 package com.ramces.ricknmortyapp.data.api
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import kotlinx.android.parcel.RawValue
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class DataResponseApi(
